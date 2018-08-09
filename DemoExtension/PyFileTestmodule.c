b8a9b3261308fea8e790a3bdece79fd49398f880
Calum Freeman <calum.freeman@diamond.ac.uk>
2018-08-09 17:26:15 +0100

    Added many more tests helpers to PyFileTestmodule.c
    PyFile_Check, PyFile_CheckExact, PyFile_FromString, PyFile_GetLine,
    PyFile_SetBufSize, PyFile_SetEncoding, PyFile_SetEncodingAndErrors,
    PyFile_SoftSpace, PyFile_IncUseCount, PyFile_DecUseCount,
    PyObject_AsFileDescriptor, PyFile_WriteObject, tp_getattro, tp_setattro,
    tp_flags, tp_doc, tp_weaklistoffset, tp_iter, tp_iternext, tp_new,
    tp_init, tp_dealloc

--- DemoExtension/PyFileTestmodule.c
+++ DemoExtension/PyFileTestmodule.c
@@ -28,7 +28,7 @@
  *
  *
  *
- * DemoExtensionmodule.c
+ * PyFileTestmodule.c
  *
  *  Created on: 2018-07-30 13:35
  *	  Author: Calum Freeman
@@ -46,29 +46,89 @@ PyMODINIT_FUNC initPyFileTest(void);
 static char X ## _docs[] = "This tests the "#X" function"
 #define MapTest(X) { "test_"#X, (PyCFunction)testPyFile_ ## X, METH_VARARGS, X ## _docs }
 
+// SetBufSize SetEncoding SetEncodingAndErrors SoftSpace
 // declare functions, doc strings and method def's
+MakeTest(PyFile_Check);
+MakeTest(PyFile_CheckExact);
 MakeTest(PyFile_WriteString);
 MakeTest(PyFile_AsFile);
 MakeTest(PyFile_Name);
-MakeTest(file_repr);
 MakeTest(PyFile_FromFile);
-
+MakeTest(PyFile_FromString);
+MakeTest(PyFile_GetLine);
+MakeTest(PyFile_SetBufSize);
+MakeTest(PyFile_SetEncoding);
+MakeTest(PyFile_SetEncodingAndErrors);
+MakeTest(PyFile_SoftSpace);
+MakeTest(PyFile_IncUseCount);
+MakeTest(PyFile_DecUseCount);
+MakeTest(PyObject_AsFileDescriptor);
+MakeTest(PyFile_WriteObject);
+MakeTest(tp_repr);
+MakeTest(tp_getattro);
+MakeTest(tp_setattro);
+MakeTest(tp_flags);
+MakeTest(tp_doc);
+MakeTest(tp_weaklistoffset);
+MakeTest(tp_iter);
+MakeTest(tp_iternext);
+MakeTest(tp_new);
+MakeTest(tp_init);
+MakeTest(tp_dealloc);
 // declare doc strings
 static char PyFile_docs[]= "this tests the PyFile API";
 
 // declare module map
 static PyMethodDef PyFileTestMethods[] = {
+		MapTest(PyFile_Check),
+		MapTest(PyFile_CheckExact),
 		MapTest(PyFile_WriteString),
 		MapTest(PyFile_AsFile),
 		MapTest(PyFile_Name),
-		MapTest(file_repr),
 		MapTest(PyFile_FromFile),
+		MapTest(PyFile_FromString),
+		MapTest(PyFile_GetLine),
+		MapTest(PyFile_SetBufSize),
+		MapTest(PyFile_SetEncoding),
+		MapTest(PyFile_SetEncodingAndErrors),
+		MapTest(PyFile_SoftSpace),
+		MapTest(PyFile_IncUseCount),
+		MapTest(PyFile_DecUseCount),
+		MapTest(PyObject_AsFileDescriptor),
+		MapTest(PyFile_WriteObject),
+		MapTest(tp_repr),
+		MapTest(tp_getattro),
+		MapTest(tp_setattro),
+		MapTest(tp_flags),
+		MapTest(tp_doc),
+		MapTest(tp_weaklistoffset),
+		MapTest(tp_iter),
+		MapTest(tp_iternext),
+		MapTest(tp_new),
+		MapTest(tp_init),
+		MapTest(tp_dealloc),
 		{ NULL, NULL, 0, NULL }
+
 };
 
 
-
 // define functions
+static PyObject* testPyFile_PyFile_Check(PyObject *self, PyObject *args){
+	PyObject *Obj;
+	if (!PyArg_ParseTuple(args, "O", &Obj)) {
+		printf("PyArg_ParseTuple in testPyFile_PyFile_Check didn't work\n");
+		return NULL;
+	}
+	return Py_BuildValue("i", PyFile_Check(Obj));
+}
+static PyObject* testPyFile_PyFile_CheckExact(PyObject *self, PyObject *args){
+	PyObject *Obj;
+	if (!PyArg_ParseTuple(args, "O", &Obj)) {
+		printf("PyArg_ParseTuple in testPyFile_PyFile_CheckExact didn't work\n");
+		return NULL;
+	}
+	return Py_BuildValue("i", PyFile_CheckExact(Obj));
+}
 static PyObject* testPyFile_PyFile_WriteString(PyObject *self, PyObject *args) {
 	PyObject *f;
 	char *myStr;
@@ -79,7 +139,7 @@ static PyObject* testPyFile_PyFile_WriteString(PyObject *self, PyObject *args) {
 
 	// TODO check that the file actually wrote successfully check with empty and non-empty files
 	int successReturn = PyFile_WriteString(myStr, f);
-	if(successReturn!=0) return Py_BuildValue("i", -1);
+	if(successReturn==0) return Py_BuildValue("i", -1);
 
 	int errorReturn = PyFile_WriteString(myStr, NULL);
 	if(errorReturn!=-1) return Py_BuildValue("i", -1);
@@ -88,11 +148,12 @@ static PyObject* testPyFile_PyFile_WriteString(PyObject *self, PyObject *args) {
 	//int javaErrorReturn = PyFile_WriteString(myStr, NULL);
 	//if(javaErrorReturn!=-2) return NULL;
 
-	free(myStr);
+	//free(myStr);
 	return Py_BuildValue("i", 1);
 }
 
 static PyObject* testPyFile_PyFile_AsFile(PyObject *self, PyObject *args){
+	// takes in a file containing a character and that character then gets a file pointer and uses it to see if the char is there, if it is then this is probably pointing to the right file
 	PyObject *Obj;
 	char myCh;
 	if (!PyArg_ParseTuple(args, "Oc", &Obj, &myCh)) {
@@ -101,45 +162,19 @@ static PyObject* testPyFile_PyFile_AsFile(PyObject *self, PyObject *args){
 	}
 	FILE *f = PyFile_AsFile(Obj);
 	if(f == NULL){
-		return Py_BuildValue("s", "fIsNULL");
+		return Py_BuildValue("s", "file is closed");
 	}
 	char ch = fgetc(f);
-	if(ch!=myCh){
-			return Py_BuildValue("i", 0);
-	}
-	return Py_BuildValue("i", 1);
+	return Py_BuildValue("c", ch);
 }
 
 static PyObject* testPyFile_PyFile_Name(PyObject *self, PyObject *args){
 	PyObject *Obj;
-	char* myName;
-	PyObject *name;
-	char* Cname;
-	if (!PyArg_ParseTuple(args, "Os", &Obj, &myName)) {
-		printf("PyArg_ParseTuple in testPyFile_PyObject_AsFileDescriptor didn't work\n");
+	if (!PyArg_ParseTuple(args, "O", &Obj)) {
+		printf("PyArg_ParseTuple in testPyFile_PyFile_Name didn't work\n");
 		return NULL;
 	}
-	PyFileObject *Test = (PyFileObject *)Obj;
-	name = PyFile_Name(Obj);
-	Cname = PyString_AsString(name);
-	if(myName!=name){
-		return Py_BuildValue("i", 0);
-	}
-	return Py_BuildValue("i", 1);
-}
-
-static PyObject* testPyFile_file_repr(PyObject *self, PyObject *args){
-	PyObject *Obj;
-	char* myStr;
-	if (!PyArg_ParseTuple(args, "Os", &Obj, &myStr)) {
-		printf("PyArg_ParseTuple in testPyFile_PyObject_AsFileDescriptor didn't work\n");
-		return NULL;
-	}
-	char* str = PyString_AsString(Obj->ob_type->tp_repr(Obj));
-	if(str!=myStr){
-		return Py_BuildValue("i", 0);
-	}
-	return Py_BuildValue("i", 1);
+	return PyFile_Name(Obj);
 }
 
 static PyObject* testPyFile_PyFile_FromFile(PyObject *self, PyObject *args){
@@ -154,6 +189,224 @@ static PyObject* testPyFile_PyFile_FromFile(PyObject *self, PyObject *args){
 	PyObject *o = PyFile_FromFile(file, Cname, Cmode, (int (*)(FILE *))NULL);
 	// TODO check the PyFile works/is what we expect
 	return Py_BuildValue("O", o);
+}
+
+static PyObject* testPyFile_PyFile_FromString(PyObject *self, PyObject *args){
+	char* Cname;
+	char* Cmode;
+	if (!PyArg_ParseTuple(args, "ss", &Cname, &Cmode)) {
+		printf("PyArg_ParseTuple in testPyFile_PyObject_AsFileDescriptor didn't work\n");
+		return NULL;
+	}
+	PyObject *o = PyFile_FromString(Cname, Cmode);
+	// TODO check the PyFile works/is what we expect
+	return Py_BuildValue("O", o);
+}
+
+static PyObject* testPyFile_PyFile_GetLine(PyObject *self, PyObject *args){
+	PyObject *Obj;
+	int n;
+	if (!PyArg_ParseTuple(args, "Oi", &Obj, &n)) {
+		printf("PyArg_ParseTuple in testPyFile_PyObject_AsFileDescriptor didn't work\n");
+		return NULL;
+	}
+	return PyFile_GetLine(Obj, n);
+}
+
+static PyObject* testPyFile_PyFile_SetBufSize(PyObject *self, PyObject *args){
+	PyObject *Obj;
+	int n;
+	if (!PyArg_ParseTuple(args, "Oi", &Obj, &n)) {
+		printf("PyArg_ParseTuple in testPyFile_PyObject_AsFileDescriptor didn't work\n");
+		return NULL;
+	}
+	PyFile_SetBufSize(Obj, n);
+	Py_RETURN_NONE; // This is just making sure it doesn't throw an error
+}
+static PyObject* testPyFile_PyFile_SetEncoding(PyObject *self, PyObject *args){
+	PyObject *Obj;
+	char *enc;
+	if (!PyArg_ParseTuple(args, "Os", &Obj, &enc)) {
+		printf("PyArg_ParseTuple in testPyFile_PyObject_AsFileDescriptor didn't work\n");
+		return NULL;
+	}
+	return Py_BuildValue("i", PyFile_SetEncoding(Obj, enc));
+}
+static PyObject* testPyFile_PyFile_SetEncodingAndErrors(PyObject *self, PyObject *args){
+	PyObject *Obj;
+	char *enc;
+	char *err;
+	if (!PyArg_ParseTuple(args, "Oss", &Obj, &enc, &err)) {
+		printf("PyArg_ParseTuple in testPyFile_PyObject_AsFileDescriptor didn't work\n");
+		return NULL;
+	}
+	;
+	return Py_BuildValue("i", PyFile_SetEncodingAndErrors(Obj, enc, err));
+}
+static PyObject* testPyFile_PyFile_SoftSpace(PyObject *self, PyObject *args){
+	PyObject *Obj;
+	int newflag;
+	if (!PyArg_ParseTuple(args, "Oi", &Obj, &newflag)) {
+		printf("PyArg_ParseTuple in testPyFile_PyObject_AsFileDescriptor didn't work\n");
+		return NULL;
+	}
+	return Py_BuildValue("i", PyFile_SoftSpace(Obj, newflag));
+}
+
+static PyObject* testPyFile_PyFile_IncUseCount(PyObject *self, PyObject *args){
+	PyObject *Obj;
+	if (!PyArg_ParseTuple(args, "O", &Obj)) {
+		printf("PyArg_ParseTuple in testPyFile_PyObject_AsFileDescriptor didn't work\n");
+		return NULL;
+	}
+	PyFile_IncUseCount((PyFileObject *)Obj);
+	return Py_BuildValue("i", 1);return NULL;
+}
+
+static PyObject* testPyFile_PyFile_DecUseCount(PyObject *self, PyObject *args){
+	PyObject *Obj;
+	if (!PyArg_ParseTuple(args, "O", &Obj)) {
+		printf("PyArg_ParseTuple in testPyFile_PyObject_AsFileDescriptor didn't work\n");
+		return NULL;
+	}
+	PyFile_DecUseCount((PyFileObject *)Obj);
+	return Py_BuildValue("i", 1);return NULL;
+}
+
+static PyObject* testPyFile_PyObject_AsFileDescriptor(PyObject *self, PyObject *args){
+	PyObject *Obj;
+	if (!PyArg_ParseTuple(args, "O", &Obj)) {
+		printf("PyArg_ParseTuple in testPyFile_PyObject_AsFileDescriptor didn't work\n");
+		return NULL;
+	}
+	return Py_BuildValue("i", PyObject_AsFileDescriptor(Obj));
+}
+
+static PyObject* testPyFile_PyFile_WriteObject(PyObject *self, PyObject *args){
+	PyObject *Obj;
+	PyObject *ToPrint;
+	int flags;
+	if (!PyArg_ParseTuple(args, "OOi", &Obj, &ToPrint, &flags)) {
+		printf("PyArg_ParseTuple in testPyFile_PyObject_AsFileDescriptor didn't work\n");
+		return NULL;
+	}
+	PyFile_WriteObject(ToPrint, Obj, flags);
+	//PyFile_WriteString("hi", Obj);
+	return Py_BuildValue("i", 1);
+}
+
+static PyObject* testPyFile_tp_repr(PyObject *self, PyObject *args){
+	PyObject *Obj;
+	if (!PyArg_ParseTuple(args, "O", &Obj)) {
+		printf("PyArg_ParseTuple in testPyFile_file_repr didn't work\n");
+		return NULL;
+	}
+	PyObject* pstr = Obj->ob_type->tp_repr(Obj);
+	char* str = PyString_AsString(pstr);
+	return Py_BuildValue("s", str);
+}
+
+static PyObject* testPyFile_tp_getattro(PyObject *self, PyObject *args){
+	PyObject *Obj;
+	char *Cname;
+	if (!PyArg_ParseTuple(args, "Os", &Obj, &Cname)) {
+		printf("PyArg_ParseTuple in testPyFile_file_repr didn't work\n");
+		return NULL;
+	}
+	PyObject *name = PyString_FromString(Cname);
+	return Obj->ob_type->tp_getattro(Obj, name);
+}
+
+static PyObject* testPyFile_tp_setattro(PyObject *self, PyObject *args){
+	PyObject *Obj;
+	char *Cname;
+	PyObject *value;
+	if (!PyArg_ParseTuple(args, "OsO", &Obj, &Cname, &value)) {
+		printf("PyArg_ParseTuple in testPyFile_file_repr didn't work\n");
+		return NULL;
+	}
+	PyObject *name = PyString_FromString(Cname);
+	int res = Obj->ob_type->tp_setattro(Obj, name, value);
+	return Py_BuildValue("i", res);
+}
+
+static PyObject* testPyFile_tp_flags(PyObject *self, PyObject *args){
+	PyObject *Obj;
+	if (!PyArg_ParseTuple(args, "O", &Obj)) {
+		printf("PyArg_ParseTuple in testPyFile_file_repr didn't work\n");
+		return NULL;
+	}
+	return Py_BuildValue("i", Obj->ob_type->tp_flags);
+}
+
+static PyObject* testPyFile_tp_doc(PyObject *self, PyObject *args){
+	PyObject *Obj;
+	if (!PyArg_ParseTuple(args, "O", &Obj)) {
+		printf("PyArg_ParseTuple in testPyFile_file_repr didn't work\n");
+		return NULL;
+	}
+	return Py_BuildValue("s", Obj->ob_type->tp_doc);
+}
+
+static PyObject* testPyFile_tp_weaklistoffset(PyObject *self, PyObject *args){
+	PyObject *Obj;
+	if (!PyArg_ParseTuple(args, "O", &Obj)) {
+		printf("PyArg_ParseTuple in testPyFile_file_repr didn't work\n");
+		return NULL;
+	}
+	return Py_BuildValue("s", "Nope");
+}
+
+static PyObject* testPyFile_tp_iter(PyObject *self, PyObject *args){
+	PyObject *Obj;
+	if (!PyArg_ParseTuple(args, "O", &Obj)) {
+		printf("PyArg_ParseTuple in testPyFile_file_repr didn't work\n");
+		return NULL;
+	}
+	return Py_BuildValue("s", Obj->ob_type->tp_iter(Obj));
+}
+
+static PyObject* testPyFile_tp_iternext(PyObject *self, PyObject *args){
+	PyObject *Obj;
+	if (!PyArg_ParseTuple(args, "O", &Obj)) {
+		printf("PyArg_ParseTuple in testPyFile_file_repr didn't work\n");
+		return NULL;
+	}
+	return Py_BuildValue("s", Obj->ob_type->tp_iternext(Obj));
+}
+
+static PyObject* testPyFile_tp_new(PyObject *self, PyObject *args){
+	PyObject *Obj;
+	if (!PyArg_ParseTuple(args, "O", &Obj)) {
+		printf("PyArg_ParseTuple in testPyFile_file_repr didn't work\n");
+		return NULL;
+	}
+	return Py_BuildValue("O", Obj->ob_type->tp_new(NULL, NULL, NULL));
+}
+static PyObject* testPyFile_tp_init(PyObject *self, PyObject *args){
+	PyObject *Obj;
+	PyObject *ags;
+	PyObject *kwags;
+	if (!PyArg_ParseTuple(args, "OOO", &Obj, &ags, &kwags)) {
+		printf("PyArg_ParseTuple in testPyFile_file_repr didn't work\n");
+		return NULL;
+	}
+	Obj->ob_type->tp_init(Obj, ags, kwags);
+	return NULL;
+}
+
+static PyObject* testPyFile_tp_dealloc(PyObject *self, PyObject *args){
+	PyObject *Obj;
+	if (!PyArg_ParseTuple(args, "O", &Obj)) {
+		printf("PyArg_ParseTuple in testPyFile_file_repr didn't work\n");
+		return NULL;
+	}
+	Obj->ob_type->tp_dealloc(Obj);
+	// After the above Obj still seems to exist, but we should be able to trust that the PyObjFree works?
+	if(Obj){
+		return Py_BuildValue("i", 1);
+	}
+	return Py_BuildValue("i", 0);
 }
 
 //initialise module
